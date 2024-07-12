33c8a24a6cebb9f77516eaf857918bd583eccf1f
Priyanshu Modi <priyanshumodi944@gmail.com>
2024-07-13 01:04:51 +0530

    update app and create dashboard controller and route

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import {Subscription} from "../models/subscription.model.js"
+import {Like} from "../models/like.model.js"
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getChannelStatus = asyncHandler(async (req, res) => {
+    // Todo: Get the channel status like total video views, total subscribers, total videos, total likes etc.
+})
+
+const getChannelVideos = asyncHandler(async (req,res) => {
+    // Todo: get all the videos uploaded by the channel
+})
+
+export {
+    getChannelStatus,
+    getChannelVideos
+}
