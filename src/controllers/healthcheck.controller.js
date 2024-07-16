8cfb0ecf9f1bf2fba6b41ac76626e67096fbef1e
Priyanshu Modi <priyanshumodi944@gmail.com>
2024-07-16 15:52:52 +0530

    build healthcheck controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -4,6 +4,10 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
     // TODO: build a healthCheck response that simply returns the OK status as json with a message
+
+    return res
+        .status(200)
+        .json(200, "health is Good :)")
 })
 
 export {
